29a2af7ae03870c06c94a808fa893ed987e8b881
Prashanth <yvprashanth@gmail.com>
2016-12-05 22:03:48 -0800

    Got 2 buttons laid out. Very simple

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -4,22 +4,30 @@
 //
 //  Created by PrashMaya2 on 12/4/16.
 //  Copyright © 2016 Prashanth Yerramilli. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var recordingLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        print(sender.currentTitle)
+        recordingLabel.text = "Recording in progress"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func stopRecording(sender: UIButton) {
+        print("Stop Recording")
+        recordingLabel.text = "Stopped recording"
+    }
 }
-
